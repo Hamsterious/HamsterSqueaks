1686462961fa6cd331918943fc35fc2a521bc709
MartinJHammer <martin_hammer@live.dk>
2017-09-09 16:10:38 +0200

    Client: Configured angular to match preffered style and structure.

--- HamsterSqueaks.Client/src/app/start/start.module.ts
+++ HamsterSqueaks.Client/src/app/start/start.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { StartComponent } from './start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    StartComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [StartComponent]
 })
-export class AppModule { }
+export class StartModule { }
