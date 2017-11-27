0a05726eb12848759d81ef290629f1d18839ef5d
MartinJHammer <martin_hammer@live.dk>
2017-11-27 17:50:59 +0100

    Client: Updated to Angular 5.

--- HamsterSqueaks.Client/src/app/start/start.module.ts
+++ HamsterSqueaks.Client/src/app/start/start.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { PostModule } from '../modules/blog/post/post.module';
-import { NavigationModule } from '../navigation/navigation.module';
+import { NavigationModule } from '../modules/navigation/navigation.module';
 import { Routing, RoutingModule } from '../routing/routing.module';
 import { StartComponent } from './start.component';
 
